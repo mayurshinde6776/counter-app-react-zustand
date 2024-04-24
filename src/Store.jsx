616e2df554a8c868dc04e6144b303e1b7cca9313
Mayur-Thinkbridge <mayurshinde6776@gmail.com>
2024-04-24 17:17:26 +0530

    Initial commit

---
+++ src/Store.jsx
@@ -0,0 +1,19 @@
+import create from 'zustand';
+
+const initialState={
+    counter:0,
+}
+
+const useStore= create((set)=> ({
+...initialState,
+increaseCounter: ()=> set((state)=>({counter:state.counter+1})),
+decreaseCounter: ()=> set((state)=>({counter:state.counter-1})),
+
+
+increaseBy: (num)=> set((state)=>({counter:state.counter+num})),
+decreaseBy: (num)=> set((state)=>({counter:state.counter-num})),
+
+reset: ()=> set(initialState),
+}));
+
+export default useStore;
